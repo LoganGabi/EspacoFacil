b7bed2f4f43a5457ef3943137a6b85a25c459c57
Gabriel Logan <142037370+LoganGabi@users.noreply.github.com>
2025-03-29 22:40:28 -0500

    Criação das models

---
+++ espacofacil_api/app/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField(max_length=100,unique=True)
+
+    def __str__(self):
+        return f'{self.name},{self.phone},{self.email}'
+
+class Equipment(models.Model):
+    nameEquipment = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return f'Nome do Equipamento: {self.nameEquipment}'
+
+class Room(models.Model):
+    roomManager = models.ForeignKey(User,on_delete=models.CASCADE)
+    nameRoom = models.CharField(max_length=100)
+    headCount = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.nameRoom} pode conter {self.headCount} indivíduos'
+
+class RoomEquipment(models.Model):
+    room = models.ForeignKey(Room,on_delete=models.CASCADE)
+    equipment = models.ForeignKey(Equipment,on_delete=models.CASCADE)
+    amount = models.IntegerField()
+
+    def __str__(self):
+        return f'O espaço armazena {self.equipment} equipamentos'
+
+class Occupancy(models.Model):
+    room = models.ForeignKey(Room,on_delete=models.CASCADE)
+    occupant = models.ForeignKey(User,on_delete=models.CASCADE)
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return f'Espaco reservado na data: {self.date}'
--- espacofacil_api/espacofacil_api/settings.py
+++ espacofacil_api/espacofacil_api/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
 
     'corsheaders',
     'rest_framework',
-    'teste'
+    'teste',
+    'app'
 ]
 
 MIDDLEWARE = [
