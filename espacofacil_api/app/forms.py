9aa02827c2667f4258a7d3c0276c1553c6ff63f4
Kauã Gabriel <81524853+kauagps@users.noreply.github.com>
2025-04-10 19:30:43 -0500

    adicao de requisicao login para navegacao

--- espacofacil_api/app/forms.py
+++ espacofacil_api/app/forms.py
@@ -6,14 +6,6 @@ class LoginForm(forms.Form):
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
-    class Meta:
-        model = User
-        fields = ['email', 'password']
-        widgets = {
-            'email': forms.EmailInput(attrs={'class': 'form-control'}),
-            'password': forms.PasswordInput(attrs={'class': 'form-control'}),
-        }
-
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
--- espacofacil_api/espacofacil_api/urls.py
+++ espacofacil_api/espacofacil_api/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from app.views import occupancy_create, occupancy_view, login_view
 from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
+
 
 
 from app.views import (
@@ -11,24 +13,26 @@ from app.views import (
     RoomSearchView)      
 
 urlpatterns = [
-    path("", login_view, name="login"),
-    path("login/", login_view, name="login"),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path("home/", RoomSearchView.as_view(), name="home"),
+    path('', RedirectView.as_view(url='/accounts/login/', permanent=False)),
     path('admin/', admin.site.urls),
-    path("roomList", RoomListView.as_view(),name="room_list"),
-    path("roomCreate", room_create,name="room_create"),
-    path("updateRoom/<int:pk>", room_update, name="room_update"),
-    path("deleteRoom/<int:pk>", RoomDeleteView.as_view(), name="room_delete"),
-    path('userList', UserListView.as_view(), name="user_list"),
-    path("userCreate", UserCreateView.as_view(), name="user_create"),
-    path("updateUser/<int:pk>", UserUpdateView.as_view(), name="user_update"),
-    path("deleteUser/<int:pk>", UserDeleteView.as_view(), name="user_delete"),
-    path("equipmentList", EquipmentListView.as_view(), name="equipment_list"),
-    path("equipmentCreate", EquipmentCreateView.as_view(),name="equipment_create"),
-    path("updateEquipmnet/<int:pk>",EquipmentUpdateView.as_view(),name="equipment_update"),
-    path("deleteEquipmnet/<int:pk>",EquipmentDeleteView.as_view(),name="equipment_delete"),
-    path("room/<int:pk>/", RoomDetailView.as_view(), name="room_detail"),
-    path("OccupancyCreate/<int:idRoom>/",occupancy_create,name="occupancy_create"),
-    path("occupancys_list/<int:idRoom>",occupancy_view,name="occupancy_list"),
+    path('accounts/', include('allauth.urls')),  # <--- Aqui
+    # path('login/', login_view, name="login"),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('home/', RoomSearchView.as_view(), name="home"),
+    path('roomList/', RoomListView.as_view(),name="room_list"),
+    path('roomCreate/', room_create,name="room_create"),
+    path('updateRoom/<int:pk>/', room_update, name="room_update"),
+    path('deleteRoom/<int:pk>/', RoomDeleteView.as_view(), name="room_delete"),
+    path('userList/', UserListView.as_view(), name="user_list"),
+    path('userCreate/', UserCreateView.as_view(), name="user_create"),
+    path('updateUser/<int:pk>/', UserUpdateView.as_view(), name="user_update"),
+    path('deleteUser/<int:pk>/', UserDeleteView.as_view(), name="user_delete"),
+    path('equipmentList/', EquipmentListView.as_view(), name="equipment_list"),
+    path('equipmentCreate/', EquipmentCreateView.as_view(),name="equipment_create"),
+    path('updateEquipmnet/<int:pk>/',EquipmentUpdateView.as_view(),name="equipment_update"),
+    path('deleteEquipmnet/<int:pk>/',EquipmentDeleteView.as_view(),name="equipment_delete"),
+    path('room/<int:pk>/', RoomDetailView.as_view(), name="room_detail"),
+    path('OccupancyCreate/<int:idRoom>/',occupancy_create,name="occupancy_create"),
+    path('occupancys_list/<int:idRoom>',occupancy_view,name="occupancy_list"),
+
 ] 
