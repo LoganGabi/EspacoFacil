8bf8a0ced65c580dbb435cd83263d7e7013839ad
Kauã Gabriel <81524853+kauagps@users.noreply.github.com>
2025-09-17 11:01:36 -0500

    build.sh makemigrations

--- espacofacil_api/build.sh
+++ espacofacil_api/build.sh
@@ -2,7 +2,26 @@
 # exit on error
 set -o errexit
 
-pip install -r requirements.txt
+echo "--- INICIANDO SCRIPT DE BUILD ---"
 
-python manage.py collectstatic --no-input
+echo "--- Instalando dependências ---"
+pip install -r requirements.txt
+echo "--- Dependências instaladas ---"
+
+# Adicionado apenas para diagnóstico
+echo "--- Executando makemigrations (para diagnóstico) ---"
+python manage.py makemigrations
+echo "--- Makemigrations finalizado ---"
+
+
+echo "--- Executando migrate ---"
 python manage.py migrate
+echo "--- Migrate finalizado ---"
+
+
+echo "--- Executando collectstatic ---"
+python manage.py collectstatic --no-input
+echo "--- Collectstatic finalizado ---"
+
+
+echo "--- SCRIPT DE BUILD CONCLUÍDO ---"
